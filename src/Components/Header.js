413417ec96b139a683e0a13fd3596202dcc3ec96
SYAN1DE <sameersony621@gmail.com>
2024-06-13 12:48:07 +0530

    v2

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css'; // Import the CSS file for styling
+ // Ensure the logo image is available in the same folder
+import bgVideo from './video/background_video.mp4';
+
+const Header = () => {
+  return (
+    <div>
+        <nav classname="p-3 flex background">
+
+        </nav>
+
+    </div>
+  );
+};
+
+export default Header;
